184a674723f0dc54f53c9eb1d21aa99e78021f82
ravi688 <rp0412204@gmail.com>
2021-11-04 15:36:06 +0530

    Added Sublime Project files and updated calltrace.h

--- include/calltrace.h
+++ include/calltrace.h
@@ -1,7 +1,30 @@
 
 #pragma once
 
-#define function_signature_void(return_type, function_name) return_type __##function_name(uint64_t __line__, const char* __function__, const char* __file__)
-#define function_signature(return_type, function_name, ...)  return_type __##function_name(__VA_ARGS__, uint64_t __line__, const char* __function__, const char* __file__)
-#define define_alias_function_void_macro(function_name) __##function_name(__LINE__, __FUNCTION__, __FILE__)
-#define define_alias_function_macro(function_name, ...) __##function_name(__VA_ARGS__, __LINE__, __FUNCTION__, __FILE__)
+#ifdef GLOBAL_DEBUG
+#define CALLTRACE_DEBUG
+#endif
+
+#ifdef GLOBAL_RELEASE
+#define CALLTRACE_RELEASE
+#endif
+
+#if !defined(CALLTRACE_DEBUG) && !defined(CALLTRACE_RELEASE)
+#	warning "None of CALLTRACE_DEBUG or CALLTRACE_RELEASE is defined; using CALLTRACE_DEBUG"
+#	define CALLTRACE_DEBUG
+#elif defined(CALLTRACE_DEBUG) && defined(CALLTRACE_RELEASE)
+#	warning "Both of CALLTRACE_DEBUG and CALLTRACE_RELEASE are defined; using CALLTRACE_DEBUG"
+#	define CALLTRACE_RELEASE
+#endif
+
+#ifdef CALLTRACE_DEBUG
+#	define function_signature_void(return_type, function_name) return_type __##function_name(uint64_t __line__, const char* __function__, const char* __file__)
+#	define function_signature(return_type, function_name, ...)  return_type __##function_name(__VA_ARGS__, uint64_t __line__, const char* __function__, const char* __file__)
+#	define define_alias_function_void_macro(function_name) __##function_name(__LINE__, __FUNCTION__, __FILE__)
+#	define define_alias_function_macro(function_name, ...) __##function_name(__VA_ARGS__, __LINE__, __FUNCTION__, __FILE__)
+#elif CALLTRACE_RELEASE
+#	define function_signature_void(return_type, function_name) return_type function_name()
+#	define function_signature(return_type, function_name, ...) return_type function_name(__VA_ARGS__)
+#	define define_alias_function_macro(function_name, ...) function_name(__VA_ARGS__)
+#	define define_alias_function_void_macro(function_name) function_name()
+#endif
