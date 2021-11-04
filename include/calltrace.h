ee1222282ca5e8c29111526d02f9f0a317faff72
ravi688 <rp0412204@gmail.com>
2021-11-04 16:40:15 +0530

    Updated calltrace.h

--- include/calltrace.h
+++ include/calltrace.h
@@ -22,7 +22,7 @@
 #	define function_signature(return_type, function_name, ...)  return_type __##function_name(__VA_ARGS__, uint64_t __line__, const char* __function__, const char* __file__)
 #	define define_alias_function_void_macro(function_name) __##function_name(__LINE__, __FUNCTION__, __FILE__)
 #	define define_alias_function_macro(function_name, ...) __##function_name(__VA_ARGS__, __LINE__, __FUNCTION__, __FILE__)
-#elif CALLTRACE_RELEASE
+#elif defined(CALLTRACE_RELEASE)
 #	define function_signature_void(return_type, function_name) return_type function_name()
 #	define function_signature(return_type, function_name, ...) return_type function_name(__VA_ARGS__)
 #	define define_alias_function_macro(function_name, ...) function_name(__VA_ARGS__)
