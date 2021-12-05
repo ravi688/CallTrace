f3b9d11607ef1d77e05ba71b5057e3af36fcdb45
ravi688 <rp0412204@gmail.com>
2021-12-05 17:53:00 +0530

    Fixed func_ptr_sig and func_ptr_sig_void

--- include/calltrace/calltrace.h
+++ include/calltrace/calltrace.h
@@ -22,8 +22,8 @@
 #	define function_signature(return_type, function_name, ...)  return_type __##function_name(uint64_t __line__, const char* __function__, const char* __file__, __VA_ARGS__)
 #	define define_alias_function_void_macro(function_name) __##function_name(__LINE__, __FUNCTION__, __FILE__)
 #	define define_alias_function_macro(function_name, ...) __##function_name(__LINE__, __FUNCTION__, __FILE__, __VA_ARGS__)
-#   define func_ptr_sig(return_type, function_name, ...) return_type (*__##function_name)(uint64_t, uint64_t, uint64_t, __VA_ARGS__)
-#   define func_ptr_sig_void(return_type, function_name) return_type (*__##function_name)(uint64_t, uint64_t, uint64_t)
+#   define func_ptr_sig(return_type, function_name, ...) return_type (*__##function_name)(uint64_t, const char*, const char*, __VA_ARGS__)
+#   define func_ptr_sig_void(return_type, function_name) return_type (*__##function_name)(uint64_t, const char*, const char*)
 #   define func_ptr(function_name) __##function_name
 #   define params(...) (__LINE__, __FUNCTION__, __FILE__, __VA_ARGS__)
 #   define no_params() (__LINE__, __FUNCTION__, __FILE__)
